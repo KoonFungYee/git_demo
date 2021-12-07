1e285329f6b6a093bd3946a4ae5f2cbb9f8e86b0
Koon Fung Yee <43363198+KoonFungYee@users.noreply.github.com>
2021-12-07 14:02:53 +0800

    first upload

---
+++ src/main/java/com/example/git_demo1/GitController.java
@@ -0,0 +1,14 @@
+package com.example.git_demo1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GitController {
+    
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+    
+}
